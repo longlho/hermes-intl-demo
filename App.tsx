67867b2706e8548734054e0b462afbe6dba9ab8c
Long Ho <longho@dropbox.com>
2020-06-24 09:27:54 -0400

    more stuff

--- App.tsx
+++ App.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import '@formatjs/intl-getcanonicallocales/polyfill'
+import '@formatjs/intl-pluralrules/polyfill'
+import '@formatjs/intl-pluralrules/dist/locale-data/ja'
+import '@formatjs/intl-numberformat/polyfill'
+import '@formatjs/intl-numberformat/dist/locale-data/ja'
+import '@formatjs/intl-datetimeformat/polyfill'
+import '@formatjs/intl-datetimeformat/dist/locale-data/ja'
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text>{new Intl.DateTimeFormat('ja', {
+      weekday: 'long',
+      era: 'long',
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+      hour12: true,
+      timeZone: 'UTC',
+      timeZoneName: 'long',
+    }).format(Date.now())}</Text>
       <Text>Open up App.tsx to start working on your app!</Text>
     </View>
   );
